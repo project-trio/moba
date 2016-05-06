b260c20cd56a1dcaa70c1c50b2697def4d46dfe0
Kyle Coburn <kylecoburn@gmail.com>
2016-05-05 21:44:23 -0700

    Triplicate minis

--- src/client/scripts/game/entity/game/wave.js
+++ src/client/scripts/game/entity/game/wave.js
@@ -10,14 +10,15 @@ module.exports = {
 		const mapWidth = Local.game.map.width();
 		const mapHeight = Local.game.map.height();
 		waveData.forEach((minionData) => {
-			const path = minionData.path;
+			const paths = minionData.paths;
 			const name = minionData.type;
 			for (let team = 0; team < 2; ++team) {
 				for (let mirror = 0; mirror < (minionData.mirror ? 2 : 1); ++mirror) {
 					let mirrored = mirror == 1;
-					// for (let mi = 0; mi < 3; ++mi) {
+					for (let pi = 0; pi < paths.length; ++pi) {
+						const path = paths[pi];
 						const minion = new Mini(team, name, path, mirrored, mapWidth, mapHeight);
-					// }
+					}
 				}
 			}
 		});
