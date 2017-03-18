821b739b45d4f8783e8783bfe6d5143c72faa456
kyis <kylecoburn@gmail.com>
2017-03-18 15:19:03 -0700

    Reload page when game is overwritten in dev env

--- src/client/play/game/events.js
+++ src/client/play/game/events.js
@@ -10,7 +10,11 @@ export default {
 		Bridge.on('update', (data) => {
 			const update = data.update
 			if (Local.game.serverUpdate != update - 1) {
-				console.error('Invalid update ' + Local.game.serverUpdate + ' ' + update)
+				if (Local.TESTING) {
+					window.location.reload(false)
+				} else {
+					console.error('Invalid update ' + Local.game.serverUpdate + ' ' + update)
+				}
 			}
 			Local.game.enqueueUpdate(update, data.moves)
 			Bridge.emit('updated', {update: update})
