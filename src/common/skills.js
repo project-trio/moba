717ac9928a8d7d4ddd15746fc13724654d3ac75e
kyis <kylecoburn@gmail.com>
2017-03-28 00:15:36 -0700

    Add Sunken skills

--- src/common/skills.js
+++ src/common/skills.js
@@ -2,6 +2,73 @@ module.exports = {
   boxy: [
   ],
   sunken: [
+    {
+      name: 'Torpedo',
+      description: 'Fires a torpedo that explodes on the first enemy target',
+      target: 2,
+      duration: 0,
+      cooldown: 150,
+      getDuration: function (level) {
+        return 0
+      },
+      getCooldown: function (level) {
+        return this.cooldown
+      },
+      start: function (index, level, ship) {
+      },
+      end: function (ship) {
+      },
+    },
+    {
+      name: 'Dive',
+      description: 'Dive down to temporarily escape enemy fire, dealing damage to enemies around you',
+      target: 1,
+      duration: 30,
+      cooldown: 200,
+      getDuration: function (level) {
+        return this.duration + level * 1
+      },
+      getCooldown: function (level) {
+        return this.cooldown - level * 10
+      },
+      start: function (index, level, ship) {
+        this.untargetable = true
+      },
+      update: function (ship, start, current, end) {
+        const elapsed = current - start
+        const transitionTime = 500
+        const diveDepth = -33
+        if (elapsed <= transitionTime) {
+          ship.shipContainer.position.z = elapsed * diveDepth / transitionTime
+        } else {
+          const remaining = end - current
+          if (remaining <= transitionTime) {
+            ship.shipContainer.position.z = remaining * diveDepth / transitionTime
+          }
+        }
+      },
+      end: function (ship) {
+        ship.shipContainer.z = 0
+        this.untargetable = false
+      },
+    },
+    {
+      name: 'Effervesce',
+      description: 'Encase yourself in a bubble, reducing incoming damage',
+      target: 1,
+      duration: 50,
+      cooldown: 150,
+      getDuration: function (level) {
+        return this.duration
+      },
+      getCooldown: function (level) {
+        return this.cooldown - level * 5
+      },
+      start: function (index, level, ship) {
+      },
+      end: function (ship) {
+      },
+    },
   ],
   glitch: [
     {
