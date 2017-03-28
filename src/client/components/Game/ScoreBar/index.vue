b6a6c2ee4402c2d50099fc40bab110c464923702
kyis <kylecoburn@gmail.com>
2017-03-27 21:43:31 -0700

    Cleanup scorebar styling

--- src/client/components/Game/ScoreBar/index.vue
+++ src/client/components/Game/ScoreBar/index.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="score-bar">
   <div class="bar-section">
-    <span class="towers team-1">{{ towers[0] }}</span> <span class="tower-symbol">〒</span> <span class="kills team-1">{{ kills[0] }}</span>
-    ⚔️
-    <span class="kills team-2">{{ kills[1] }}</span> <span class="tower-symbol">〒</span> <span class="towers team-2"> {{ towers[1] }}</span>
+    <span class="towers team-1">{{ towers[0] }}</span> <span class="tower-symbol">〒</span>
+    <span class="kills-container">
+      <span class="kills team-1">{{ kills[0] }}</span>
+      ⚔️
+      <span class="kills team-2">{{ kills[1] }}</span> <span class="tower-symbol">〒</span>
+    </span>
+    <span class="towers team-2"> {{ towers[1] }}</span>
   </div>
 </div>
 </template>
@@ -32,10 +36,14 @@ export default {
   top 0
   font-size 1.2em
   font-weight 600
+  text-shadow 0 1px 1px #444
 
 .bar-section
-  padding-left 16px
-  padding-right 16px
+  padding-left 20px
+  padding-right 20px
+
+.kills-container
+  margin 0 8px
 
 .kills
   font-size 2em
