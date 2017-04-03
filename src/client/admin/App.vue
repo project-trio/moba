7dbb3a464e331f183b1cf801c40f99d62df07779
kyis <kylecoburn@gmail.com>
2017-04-02 21:02:05 -0700

    Display player info on admin page

--- src/client/admin/App.vue
+++ src/client/admin/App.vue
@@ -1,18 +1,16 @@
 <template>
 <div id="app" class="inherit">
-  <h1>Players</h1>
-  <div class="players">
-    {{ players.length - 1 }} players
-  </div>
-  <h1>Games</h1>
-  <div class="players">
-    {{ games.length }} games
-  </div>
+  <h1>{{ players.length }} online</h1>
+  <div>{{ players.join(', ') }}</div>
+  <h1>{{ games.length }} games</h1>
   <div v-for="game in games" class="game">
     <div>Status: {{ game.state }}</div>
     <div>Size: {{ game.size }}</div>
-    <div>Started: {{ game.started }}</div>
-    <div>Update #: {{ game.serverUpdate }}</div>
+    <div>Update: {{ game.update }}</div>
+    Players:
+    <div v-for="player in game.players">
+      {{ player.team }} {{ player.name }} {{ player.shipName }}
+    </div>
   </div>
 </div>
 </template>
@@ -33,7 +31,8 @@ export default {
     Bridge.on('auth', () => {
       Bridge.emit('admin', 'get', (response) => {
         this.games = response.games
-        this.players = response.players
+        console.log(response.games)
+        this.players = response.names
       })
     })
   },
