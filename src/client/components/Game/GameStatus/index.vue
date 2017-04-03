8988c9a990d82ea527e6682ed85e5008308833ac
kyis <kylecoburn@gmail.com>
2017-04-03 08:52:30 -0700

    Refactor GameStatus overlay to component, add continue button to game over

---
+++ src/client/components/Game/GameStatus/index.vue
@@ -0,0 +1,106 @@
+<template>
+<div class="game-status">
+  <div v-if="showHelp" class="bar-section panel">
+    <h1>Help</h1>
+    <p>Click the gear icon to toggle quality mode (disables shadows, antialiasing, etc.)</p>
+    <h2>Hotkeys</h2>
+    <p>Activate skills with <b>1</b>, <b>2</b>, <b>3</b></p>
+    <p>Upgrade with <b>shift</b>, <b>control</b>, or <b>alt</b> + <b>1</b>, <b>2</b>, <b>3</b></p>
+    <p>Press <b>enter</b> to chat with your team</p>
+  </div>
+  <div v-else-if="winningTeam !== null">
+    <div class="bar-section panel">
+      <h1>game over</h1>
+      <h2 :class="`team-${winningTeam + 1}`">team {{ winningTeamColor }} won!</h2>
+      <button @click="onReturnToLobby" class="panel-button">continue</button>
+    </div>
+    <div class="bar-section panel">
+      <div v-for="i in 5">
+        {{ i }}
+      </div>
+    </div>
+  </div>
+  <div v-else-if="missingUpdate" class="bar-section panel">
+    <h1>waiting for server connection</h1>
+  </div>
+  <div v-else-if="playing">
+    <h1 v-if="reemergeIn !== null">respawn in {{ reemergeIn }}</h1>
+  </div>
+</div>
+</template>
+
+<script>
+import router from '@/router'
+import store from '@/store'
+
+import Local from '@/play/local'
+
+import Loop from '@/play/render/loop'
+
+export default {
+  components: {
+
+  },
+
+  computed: {
+    showHelp () {
+      return store.state.game.showHelp
+    },
+    missingUpdate () {
+      return store.state.game.missingUpdate
+    },
+    playing () {
+      return store.state.game.playing
+    },
+    winningTeam () {
+      return store.state.game.winningTeam || 1
+    },
+    winningTeamColor () {
+      return this.winningTeam === 0 ? 'blue' : 'pink'
+    },
+
+    renderTime () {
+      return store.state.game.renderTime
+    },
+
+    reemergeIn () {
+      if (store.state.local.reemergeAt) {
+        const diff = store.state.local.reemergeAt - this.renderTime
+        if (diff >= 0) {
+          return Math.round(diff / 1000)
+        }
+      }
+      return null
+    },
+  },
+
+  methods: {
+    onReturnToLobby () {
+      router.replace({ name: 'Lobby' })
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.game-status
+  display flex
+  align-items center
+  justify-content center
+  position absolute
+  top 0
+  left 0
+  right 0
+  bottom 0
+  margin auto
+  padding-top 34px
+  color #fffffe
+  text-shadow -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000
+  pointer-events none
+
+.panel
+  padding 0 16px
+  pointer-events none
+  width 480px
+  max-width 100%
+</style>
--- src/client/components/Game/index.vue
+++ src/client/components/Game/index.vue
@@ -2,26 +2,7 @@
 <div class="inherit">
   <canvas id="canvas" class="inherit"></canvas>
 
-  <div class="game-status">
-    <div v-if="showHelp" class="bar-section panel">
-      <h1>Help</h1>
-      <p>Click the gear icon to toggle quality mode (disables shadows, antialiasing, etc.)</p>
-      <h2>Hotkeys</h2>
-      <p>Activate skills with <b>1</b>, <b>2</b>, <b>3</b></p>
-      <p>Upgrade with <b>shift</b>, <b>control</b>, or <b>alt</b> + <b>1</b>, <b>2</b>, <b>3</b></p>
-      <p>Press <b>enter</b> to chat with your team</p>
-    </div>
-    <div v-else-if="winningTeam !== null" class="bar-section panel">
-      <h1>game over</h1>
-      <h2 :class="`team-${winningTeam + 1}`">team {{ winningTeamColor }} won!</h2>
-    </div>
-    <div v-else-if="missingUpdate" class="bar-section panel">
-      <h1>waiting for server connection</h1>
-    </div>
-    <div v-else-if="playing">
-      <h1 v-if="reemergeIn !== null">respawn in {{ reemergeIn }}</h1>
-    </div>
-  </div>
+  <game-status></game-status>
 
   <unit-select v-if="!playing && winningTeam === null"></unit-select>
 
@@ -37,12 +18,14 @@ import Local from '@/play/local'
 
 import Loop from '@/play/render/loop'
 
+import GameStatus from '@/components/Game/GameStatus'
 import PlayerBar from '@/components/Game/PlayerBar'
 import ScoreBar from '@/components/Game/ScoreBar'
 import UnitSelect from '@/components/Game/UnitSelect'
 
 export default {
   components: {
+    GameStatus,
     PlayerBar,
     ScoreBar,
     UnitSelect,
@@ -61,34 +44,12 @@ export default {
   },
 
   computed: {
-    showHelp () {
-      return store.state.game.showHelp
-    },
-    missingUpdate () {
-      return store.state.game.missingUpdate
-    },
     playing () {
       return store.state.game.playing
     },
+
     winningTeam () {
       return store.state.game.winningTeam
-    },
-    winningTeamColor () {
-      return this.winningTeam === 0 ? 'blue' : 'pink'
-    },
-
-    renderTime () {
-      return store.state.game.renderTime
-    },
-
-    reemergeIn () {
-      if (store.state.local.reemergeAt) {
-        const diff = store.state.local.reemergeAt - this.renderTime
-        if (diff >= 0) {
-          return Math.round(diff / 1000)
-        }
-      }
-      return null
     },
   },
 }
@@ -105,31 +66,10 @@ export default {
   color #fffffe
   pointer-events none
 
-.game-status
-  display flex
-  align-items center
-  justify-content center
-  position absolute
-  top 0
-  left 0
-  right 0
-  bottom 0
-  margin auto
-  padding-top 34px
-  color #fffffe
-  text-shadow -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000
-  pointer-events none
-
 .bar-section
   background rgba(80, 80, 80, 0.85)
   margin 0 8px
   padding 8px
   box-sizing border-box
   pointer-events auto
-
-.panel
-  padding 0 16px
-  pointer-events none
-  width 480px
-  max-width 100%
 </style>
