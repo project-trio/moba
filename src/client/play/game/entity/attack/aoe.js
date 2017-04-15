4388dce1dcc305dbc6e693c269c927667f675de9
kyis <kylecoburn@gmail.com>
2017-04-15 12:43:38 -0700

    Prevent aoe applying to dead targets

--- src/client/play/game/entity/attack/aoe.js
+++ src/client/play/game/entity/attack/aoe.js
@@ -47,7 +47,7 @@ class AreaOfEffect {
     const fromUnit = this.source
     for (let idx = units.length - 1; idx >= 0; idx -= 1) {
       const target = units[idx]
-      if (target.tower && !this.hitsTowers) {
+      if (target.isDead || (target.tower && !this.hitsTowers)) {
         continue
       }
       const isAlly = fromUnit.alliedTo(target)
